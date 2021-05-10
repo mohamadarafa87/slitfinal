83afffef016edaf3d0bf701be49f512689fbcc75
mohamadarafa87 <82479352+mohamadarafa87@users.noreply.github.com>
2021-05-10 06:32:41 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.tree import DecisionTreeClassifier ,DecisionTreeRegressor
+
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve,explained_variance_score
 from sklearn.metrics import precision_score, recall_score
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import KFold
@@ -16,27 +18,44 @@ from collections import Counter
 import matplotlib.pyplot as plt
 
 def main():
-    st.title("Your Binary Classification Web App")
-    st.sidebar.title("Binary Classification Web App")
-    st.markdown("Any risk of Heart Attack??")
-    st.sidebar.markdown("Check the Heart's Health")
+    st.title("Your Binary Model Web App")
+    st.sidebar.title("Binary Model Web App")
+    st.markdown("Determine the accurate Model's algorithm ")
+    st.sidebar.markdown("Model accuracy is affected by Outlier")
+
+
+    st.sidebar.subheader("Select The supervised learning mode")
+    SuperVised = st.sidebar.selectbox("", ('Classification','Regression'), key='SuperVised')
+
 
     st.sidebar.subheader("Select splitting mode")
-    CrosVal = st.sidebar.radio("True: Cross Validation, False: Splitting", ('True', 'False'), key='CrosVal')
-
+    CrosVal = st.sidebar.checkbox("Cross Validation", True)
 
 
     @st.cache(persist=True)
     def load_data():
-        df = pd.read_csv('data/cardio1.csv')
+
+
+
+        df = pd.read_csv('C:\\Users\\Dell\\Desktop\\Pr1\\data\\cardio1.csv')
         return df
 
     @st.cache(persist=True)
     def split(df):
+
+            my_features = ['age_year', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol', 'gluc', 'smoke', 'alco','active']
+            Input = df[my_features]
+
             labels = df['cardio']
 
-            my_features = ['age_year', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol', 'gluc', 'smoke', 'alco',
-                           'active']
+
+
+            reg_feature = ['age_year']
+            inputReg = df[reg_feature]
+
+            labelReg = df['ap_hi']
+
+
 
             # convert features into numeric
             df['age_year'] = pd.to_numeric(df['age_year'], errors='coerce')
@@ -54,21 +73,30 @@ def main():
             df = df.replace(np.nan, 1, regex=True)
             df = df.fillna(1)
 
-            Input = df[my_features]
-            if  CrosVal == ['False']:
-                x_train, x_test, y_train, y_test = train_test_split(Input, labels, test_size=0.3, random_state=0)
-                return x_train, x_test, y_train, y_test
+
+            # ---------------------------------------------------------------------#
+            if SuperVised == ['Regression']:
+                kfold = KFold(n_splits=10, random_state=40, shuffle=True)
+                for train_index, test_index in kfold.split(inputReg):
+                    x_train, x_test, y_train, y_test = inputReg.iloc[train_index], inputReg.iloc[test_index], \
+                                                       labelReg.iloc[train_index], labelReg.iloc[test_index]
+                    return x_train, x_test, y_train, y_test
             else:
-                 kfold = KFold(n_splits=10, random_state=40, shuffle=True)
-                 for train_index, test_index in kfold.split(Input):
-                     x_train, x_test, y_train, y_test = Input.iloc[train_index], Input.iloc[test_index],\
-                                                        labels.iloc[train_index], labels.iloc[test_index]
-                     return x_train, x_test, y_train, y_test
+                # CrosVal = st.sidebar.checkbox("Cross Validation") ==False
+                if CrosVal == 'False':
+                        x_train, x_test, y_train, y_test = train_test_split(Input, labels, test_size=0.3, random_state=0)
+                        return x_train, x_test, y_train, y_test
+                else:
+                    kfold = KFold(n_splits=10, random_state=40, shuffle=True)
+                    for train_index, test_index in kfold.split(Input):
+                        x_train, x_test, y_train, y_test = Input.iloc[train_index], Input.iloc[test_index],\
+                                                               labels.iloc[train_index], labels.iloc[test_index]
+                        return x_train, x_test, y_train, y_test
 
 
+    #                st.sidebar.subheader("Select splitting mode")
 
-
-
+    # ------------------------------------------------------------------
 
 
     def plot_metrics(metrics_list):
@@ -83,28 +111,29 @@ def main():
             st.subheader("ROC Curve")
             plot_roc_curve(model, x_test, y_test, alpha=0.8)
             st.pyplot()
+        if 'Precision-Recall Curve' in metrics_list:
+            st.subheader("Precision-Recall Curve")
+            plot_precision_recall_curve(model, x_test, y_test)
+            st.pyplot()
 
-
-
+# klearn.metrics.explained_variance_score(y_true, y_pred, *, sample_weight=None, multioutput='uniform_average')
     df = load_data()
     class_names = ['no-stroke', 'stroke']
 
     x_train, x_test, y_train, y_test = split(df)
 
-  #  st.sidebar.subheader("select Smote")
-  #   smote = st.sidebar.selectbox("Smote", ("SMOTE", "NOSMOTE"))
-
-
 
     over = SMOTE()
     x_train1, y_train1 = over.fit_resample(x_train, y_train)
 
-
-
-
-    st.sidebar.subheader("Select Classifier")
-    classifier = st.sidebar.selectbox("Choose one Classifier",
-                                      ("Random Forest","Logistic Regression", "Decision Tree Classifier","KNNeighbors Classifier"))
+    if SuperVised =='Classification':
+        st.sidebar.subheader("Select Classifier")
+        classifier = st.sidebar.selectbox("Choose one Classifier",
+                                          ("Random Forest","Logistic Regression", "Decision Tree Classifier","KNNeighbors Classifier"))
+    if SuperVised =='Regression':
+        st.sidebar.subheader("Select Model")
+        classifier = st.sidebar.selectbox("Choose one Classifier",
+                                          ("Decision Tree Regression","Random Forest Regression","Linear Regression"))
 
 
     if classifier == 'Logistic Regression':
@@ -113,9 +142,9 @@ def main():
         max_iter = st.sidebar.slider("Maximum number of iterations", 100, 500, key='max_iter')
         Fimportance = st.sidebar.radio("Select importance", ('False','True'), key='Fimportance')
         metrics = st.sidebar.multiselect("What metrics to plot?",
-                                         ('Confusion Matrix', 'ROC Curve'))
+                                         ('Confusion Matrix', 'ROC Curve','Precision-Recall Curve'))
 
-        if  st.sidebar.button("Classfiy", key='classify'):
+        if  st.sidebar.button("Classify", key='classify'):
             st.subheader("Logistic Regression Results")
             model = LogisticRegression(C=C, max_iter=max_iter)
             model.fit(x_train1, y_train1)
@@ -140,11 +169,11 @@ def main():
                                                key='n_estimators')
         max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 100, step=1, key='max_depth')
        # bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ('True', 'False'), key='bootstrap')
-        smote     = st.sidebar.radio("smote??", ('True', 'False'), key='smote')
+        smote     = st.sidebar.radio("SMOTE", ('True', 'False'), key='smote')
         metrics = st.sidebar.multiselect("What metrics to plot?",
-                                         ('Confusion Matrix', 'ROC Curve'))
+                                         ('Confusion Matrix', 'ROC Curve','Precision-Recall Curve'))
 
-        if st.sidebar.button("Classfiy", key='classify'):
+        if st.sidebar.button("Classify", key='classify'):
             st.subheader("")
             model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth ,n_jobs=-1) # , bootstrap=bootstrap)
             if smote == 'True':
@@ -164,11 +193,11 @@ def main():
     if classifier == 'Decision Tree Classifier':
         st.sidebar.subheader("Classifier's Parameters")
         metrics = st.sidebar.multiselect("What metrics to plot?",
-                                         ('Confusion Matrix', 'ROC Curve'))
+                                         ('Confusion Matrix', 'ROC Curve','Precision-Recall Curve'))
 
-        if st.sidebar.button("Classfiy", key='classify'):
+        if st.sidebar.button("Classify", key='classify'):
             st.subheader("Decision Tree Classifier Results")
-            model = LogisticRegression() #   (C=C, max_iter=max_iter)
+            model = DecisionTreeClassifier() #   (C=C, max_iter=max_iter)
             model.fit(x_train1, y_train1)
             accuracy1 = model.score(x_train1, y_train1)
             accuracy = model.score(x_test, y_test)
@@ -183,9 +212,9 @@ def main():
         st.sidebar.subheader("Classifier's Parameters")
         n_neighbors = st.sidebar.number_input("The number of neighbours in the classifier", 1, 10, step=1,key='n_neighbors')
         metrics = st.sidebar.multiselect("What metrics to plot?",
-                                         ('Confusion Matrix', 'ROC Curve'))
+                                         ('Confusion Matrix', 'ROC Curve','Precision-Recall Curve'))
 
-        if st.sidebar.button("Classfiy", key='classify'):
+        if st.sidebar.button("Classify", key='classify'):
             st.subheader("KNNeighbors Classifier Results")
             model = KNeighborsClassifier(n_neighbors=n_neighbors)
             model.fit(x_train1, y_train1)
@@ -198,12 +227,79 @@ def main():
             st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(4))
             plot_metrics(metrics)
 
+# -Regression---------------------------------------------------------------------------------Linear-Regression
+
+    if classifier == 'Linear Regression':
+        if  st.sidebar.button("Regress", key='classify'):
+            st.subheader("Linear Regression Results")
+            model = LinearRegression()
+            model.fit(x_train1, y_train1)
+            accuracy1 = model.score(x_train1, y_train1)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy on train ", accuracy1.round(4))
+            st.write("Accuracy on test", accuracy.round(4))
+#            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(4))
+#            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(4))
+            st.write('The explained variance of the Linear Regression model of Cardio data is:',
+            explained_variance_score(y_test, y_pred))
+
+           # ------------------------------------ ----------------------------------------------------------Random-Forest-Regression-----
+    if classifier == 'Random Forest Regression':
+        st.sidebar.subheader("Regressor's Parameters")
+        n_estimators = st.sidebar.number_input("The number of trees in the forest", 10, 100, step=1,
+                                               key='n_estimators')
+        max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 100, step=1, key='max_depth')
+       # bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ('True', 'False'), key='bootstrap')
+        smote     = st.sidebar.radio("SMOTE", ('True', 'False'), key='smote')
+
+        if st.sidebar.button("Regress", key='classify'):
+            st.subheader("")
+            model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth ,n_jobs=-1) # , bootstrap=bootstrap)
+            if smote == 'True':
+               model.fit(x_train1, y_train1)
+            else:
+                 model.fit(x_train, y_train)
+            accuracy1 = model.score(x_train1, y_train1)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy on train ", accuracy1.round(4))
+            st.write("Accuracy on test", accuracy.round(4))
+#            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(4))
+#            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(4))
+            st.write('The explained variance of the Random Forest Regression model of Cardio data is:',
+            explained_variance_score(y_test, y_pred))
+
+           # ------------------------------------ ----------------------------------------------------------Decision-Tree-Regression-----
+    if classifier == 'Decision Tree Regression':
+        st.sidebar.subheader("Regressor's Parameters")
+        DTR_max_dep = st.sidebar.number_input("max_depth", 1, 500,  step=1, key='DTR_max_dep')
+        DTR_min_samples_split = st.sidebar.number_input("min_samples_split", 1, 500, key='min_samples_split')
+
+        if st.sidebar.button("Regress", key='classify'):
+            st.subheader("Decision Tree Classifier Results")
+            model = DecisionTreeRegressor(max_depth=DTR_max_dep, min_samples_split=DTR_min_samples_split) #   (C=C, max_iter=max_iter)
+            model.fit(x_train1, y_train1)
+            accuracy1 = model.score(x_train1, y_train1)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy on train ", accuracy1.round(4))
+            st.write("Accuracy on test", accuracy.round(4))
+#            st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(4))
+#            st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(4))
+            st.write('The explained variance of the Decision Tree Regression model of Cardio data is:',
+            explained_variance_score(y_test, y_pred))
+
+
+# -----------------------------------------------------------------------end of Regression
+
+
     if st.sidebar.checkbox("Show raw data", False):
        st.subheader("Cardio Data Set (Classification)")
        st.write(df)
 
 
-    st.sidebar.subheader("Data on SMOTE")
+    st.sidebar.subheader("SMOTE: Synthetic Minority Oversampling Technique")
     st.sidebar.write("Date Before SMOTE ", Counter(y_train))
     st.sidebar.write("Date After SMOTE ", Counter(y_train1))
 
